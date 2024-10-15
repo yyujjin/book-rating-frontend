8792f3aae11225986da7af57adb0bc75a33fe3e3
suhyeoonn <suhyeon1740@gmail.com>
2024-10-15 22:46:39 +0900

    💡 api 리버스 프록시 설정 주석처리
    백엔드에서 CORS 허용하여 더 이상 불필요

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,14 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 const nextConfig = {
   output: "export",
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${API_URL}/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${API_URL}/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
