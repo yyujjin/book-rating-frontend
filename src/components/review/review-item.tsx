1ebca68d0ee83b9d81b0d3a38973ea5af26f6b01
suhyeoonn <suhyeon1740@gmail.com>
2024-11-17 05:07:39 +0900

    🎨 리뷰 수정 버튼 숨김처리

--- src/components/review/review-item.tsx
+++ src/components/review/review-item.tsx
@@ -7,6 +7,12 @@ import Rating from "../star-group";
 import { useState } from "react";
 import ReviewEditForm from "./review-edit-form";
 import { useReview } from "@/lib/hooks/review";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function BookReview({
   bookId,
@@ -41,8 +47,9 @@ export default function BookReview({
       <div className="space-y-2 flex-1">
         <div className="flex items-center justify-between">
           <Rating rating={review.rating} />
-          <div className="flex items-center gap-1">
-            <Button
+          <div className="flex items-center gap-1 cursor-not-allowed">
+            {/* 구글 로그인 될때까지 수정 기능 막기 */}
+            {/* <Button
               size="sm"
               variant="ghost"
               className="text-muted-foreground hover:bg-muted/10"
@@ -50,16 +57,26 @@ export default function BookReview({
             >
               <FilePenIcon className="w-4 h-4" />
               <span className="sr-only">Edit</span>
-            </Button>
-            <Button
-              size="sm"
-              variant="ghost"
-              className="text-muted-foreground hover:bg-muted/10"
-              onClick={deleteHandler}
-            >
-              <TrashIcon className="w-4 h-4" />
-              <span className="sr-only">Delete</span>
-            </Button>
+            </Button> */}
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Button
+                    size="sm"
+                    variant="ghost"
+                    className="text-muted-foreground hover:bg-muted/10 disabled:cursor-not-allowed"
+                    onClick={deleteHandler}
+                    disabled={true}
+                  >
+                    <TrashIcon className="w-4 h-4 cursor-not-allowed" />
+                    <span className="sr-only">Delete</span>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  관리자 권한만 삭제할 수 있습니다.
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
         </div>
         {isEditMode ? (
