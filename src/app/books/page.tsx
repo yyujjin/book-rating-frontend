3816c57eb6ca3e7cf12f6fb16042851315f8022d
suhyeoonn <suhyeon1740@gmail.com>
2024-11-18 23:35:59 +0900

    💄 책 추가 버튼 UI 변경

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -14,26 +14,22 @@ export default function Home() {
   return (
     <>
       <div className="container mx-auto py-8 px-4 md:px-6">
-        {/* TODO: */}
-        {/* <div className="flex items-center justify-between mb-6">
-          <SearchInput />
-        </div> */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          <div className="relative overflow-hidden rounded-lg shadow-lg group hover:shadow-xl hover:-translate-y-2 transition-transform duration-300 ease-in-out cursor-pointer">
+        <div className="flex items-center justify-start mb-6">
+          {/* TODO: */}
+          {/* <SearchInput /> */}
+          <Button
+            asChild
+            size="lg"
+            variant="default"
+            className="transition-transform duration-300 text-white border border-gray-200 shadow-lg bg-gradient-to-r from-pink-400 to-orange-400 font-bold hover:-translate-y-2"
+          >
             <Link href="/books/add">
-              <div className="flex items-center justify-center h-full">
-                <Button
-                  size="lg"
-                  variant="ghost"
-                  className="transition-transform duration-300 text-gray-700 hover:bg-primary/10 group-hover:scale-125 group-hover:text-gray-800"
-                >
-                  <PlusIcon className="w-6 h-6" />
-                  Add Book
-                </Button>
-              </div>
+              <PlusIcon className="w-6 h-6" />
+              Add Book
             </Link>
-          </div>
-
+          </Button>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           <BookList />
         </div>
       </div>
